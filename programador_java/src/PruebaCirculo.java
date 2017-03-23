89967825111b3160cf276fa409b8315e62c17d4a
JCarlos <JCarlos@DESKTOP-5OBNMB0>
2017-03-23 22:18:22 +0000

    Proejcto Java 2

---
+++ programador_java/src/PruebaCirculo.java
@@ -0,0 +1,35 @@
+
+
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
+public class PruebaCirculo {
+
+	public static void main(String[] args) {
+
+		Circulo circulo = new Circulo(37, 43, 2.5);
+		String salida = "La coordenada X es " + micirculito(circulo).obtenerX() +
+		"\nLa coordenada Y es " + micirculito(circulo).obtenerY() +
+		"\nEl radio es " + micirculito(circulo).obtenerRadio();
+		micirculito(circulo).establecerX(35);
+		micirculito(circulo).establecerY(20);
+		micirculito(circulo).establecerRadio(4.25);
+		salida += "\n\nLa nueva ubicación y el radio del círculo son\n" +
+		micirculito(circulo).toString();
+		DecimalFormat dosDigitos = new DecimalFormat("0.00");
+		salida += "\nEl diámetro es " +
+		dosDigitos.format(micirculito(circulo).obtenerDiametro());
+		salida += "\nLa circunferencia es " +
+		dosDigitos.format(micirculito(circulo).obtenerCircunferencia());
+		salida += "\nEl área es " + dosDigitos.format(micirculito(circulo).obtenerArea());
+		JOptionPane.showMessageDialog(null, salida);
+		System.exit(0);
+
+	}
+
+	private static Circulo micirculito(Circulo circulo) {
+		return circulo;
+	} 
+
+} 
+
