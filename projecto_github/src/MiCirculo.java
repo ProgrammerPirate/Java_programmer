2062652d2907509d95f6a3a17f32d94e9eec2aec
JCarlos <JCarlos@DESKTOP-5OBNMB0>
2017-03-23 20:54:55 +0000

    Projecto_github

---
+++ projecto_github/src/MiCirculo.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Scanner;
+
+public class MiCirculo {
+
+	
+  public static void main(String[] args) {
+	int x,y; 
+	double radio, diametro, circunferencia, area;
+	
+  	Scanner sc = new Scanner(System.in);
+  	System.out.println("Punto x: ");
+	x= sc.nextInt();
+	System.out.println("Punto y: ");
+	y = sc.nextInt();
+	System.out.println("Radio: ");
+	radio = sc.nextInt();
+	diametro = 2 * Diametro(radio);
+	circunferencia= Diametro(diametro) * 3.1416;
+	area= 3.1416 * Diametro(radio) * Diametro(radio);
+	System.out.println("Los datos de mi círculo son Centro:" + x + "," + y + ", Radio: "+ Diametro(radio) + ", Diámetro: " + Diametro(diametro) 
+			+ ", Circunferencia: " + Diametro(circunferencia) + ", Area: "+ Diametro(area));
+	sc.close();
+  }
+
+private static double Diametro(double diametro) {
+	return diametro;
+}
+}
